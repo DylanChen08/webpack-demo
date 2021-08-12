9803dc6e0c1ac79161cfb313cc515731305f5309
DC <easetreychen@gmail.com>
2021-08-12 19:14:23 +0800

    Prevent Duplication- dependOn: 'shared' :配置优化,避免重复

--- webpack.config.js
+++ webpack.config.js
@@ -10,17 +10,20 @@ module.exports = {
     },
     //配置多入口
     entry: {
-        index: './src/index.js',
-        print: './src/print.js'
+        // index: './src/index.js',
+        // print: './src/print.js',
+        // another: './src/another-module.js',
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared',
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared',
+        },
+        shared: 'lodash', //设置共享模块，防止重复引入
 
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            // title: 'Output Management',
-            title: 'development',
-        }),
-    ],
-    output: {
+    }, output: {
         // filename: 'main.js',
         // filename: 'bundle.js',
         filename: '[name].bundle.js',
@@ -28,6 +31,16 @@ module.exports = {
         clean: true,
         publicPath: '/',
     },
+    optimization: { //优化设置项
+        runtimeChunk: 'single',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // title: 'Output Management',
+            title: 'development',
+        }),
+    ],
+
     module: {
         rules: [
             {
